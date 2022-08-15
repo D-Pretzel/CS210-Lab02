6327ec35b395639de4d9505e8a73e033a78033a2
David Petzold <C25David.Petzold@afacademy.af.edu>
2022-08-14 19:04:59 -0600

    Outputs

--- output.c
+++ output.c
@@ -1,12 +1,21 @@
 /* output.c
  * ===========================================================
- * Name: 
- * Section:
+ * Name: David Petzold
+ * Section: T6/7
  * Project: Lab 2 - Output
  * Purpose: Learn C basics
  */
 
 // What do you need to #include to use the printf/scanf functions?
-
+#include <stdio.h>
 
 // Put Your Main Function Here
+int main(void) {
+
+    printf("Name:\t\tCadet David Petzold\n");
+    printf("Hometown:\tSt. Cloud, MN\n");
+    printf("Squadron:\t25 Rock Hard Redeye\n");
+    printf("Fact:\t\tI fly gliders!\n");
+
+    return 0;
+}
